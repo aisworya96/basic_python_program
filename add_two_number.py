b02961c82a8bf7a3f4f5892911eba1383731245a
apple <aisworyapanda@gmail.com>
2025-01-17 11:47:03 +0530

    New Functionality added to the file

--- add_two_number.py
+++ add_two_number.py
@@ -1,8 +1,17 @@
-def add(a,b):
-    return a+b
-num1 = 10
-num2 = 5
-sum_of_twonumbers = add(num1,num2)
+# def add(a,b):
+#     return a+b
+# num1 = 10
+# num2 = 5
+# sum_of_twonumbers = add(num1,num2)
+#
+# print("Sum of {0} and {1} is {2};" .format(num1,
+#                                            num2, sum_of_twonumbers))
 
-print("Sum of {0} and {1} is {2};" .format(num1,
-                                           num2, sum_of_twonumbers))
+# Add Two Number Using Lambda Function
+
+add_numbers = lambda x, y: x + y
+
+num1 = 5
+num2 = 7
+result = add_numbers(num1, num2)
+print(result)
