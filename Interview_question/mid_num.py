8d9954db39fa2cf2464f5aeb765254708902e7fd
apple <aisworyapanda@gmail.com>
2025-02-03 21:17:50 +0530

    New commit2

--- Interview_question/mid_num.py
+++ Interview_question/mid_num.py
@@ -12,3 +12,13 @@ string = ''.join(lst)
 
 print(string)
 print(type(string))
+
+# Adding Two List Elements Together
+
+lst1 = [1, 2, 3]
+lst2 = [4, 5, 6]
+
+res_lst = []
+for i in range(0, len(lst1)):
+    res_lst.append(lst1[i] + lst2[i])
+print(res_lst)
