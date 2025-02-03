f4ea73c272a6771f315b9b690e69f028d63caed0
apple <aisworyapanda@gmail.com>
2025-02-03 21:24:59 +0530

    New commit4

---
+++ Leetcode/roman_integer.py
@@ -0,0 +1,26 @@
+def roman_to_int(s):
+    roman_values = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    total = 0
+    prev_value = 0
+
+    for char in reversed(s):
+        value = roman_values[char]
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        prev_value = value
+
+    return total
+
+roman_numeral = "MCMXCIV"
+print(f"The integer value of {roman_numeral} is {roman_to_int(roman_numeral)}")
