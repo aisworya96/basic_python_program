8cb57cdca864d5cb20e757456c643f280c8fba80
apple <aisworyapanda@gmail.com>
2025-04-14 22:27:53 +0530

    modified2

--- Leetcode/longest_prefix.py
+++ Leetcode/longest_prefix.py
@@ -1,3 +1,19 @@
 # Write a function to find the longest common prefix string amongst an array of strings.
 
 # If there is no common prefix, return an empty string "".
+def longest_common_prefix(strs):
+    if not strs:
+        return ""
+
+    # Start with the first string as the prefix
+    prefix = strs[0]
+
+    # Compare the prefix with each string
+    for s in strs[1:]:
+        # Shorten the prefix until it matches the start of s
+        while not s.startswith(prefix):
+            prefix = prefix[:-1]
+            if not prefix:
+                return ""
+
+    return prefix
